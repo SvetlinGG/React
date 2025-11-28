85b3020bea1012d2081f91a5aeaa7c9887483efe
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-29 00:47:51 +0100

    instal dev dependencies plugin-react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 
+
 const rootHtmlElement = document.getElementById('root');
 const rootReactElement = ReactDom.createRoot(rootHtmlElement);
 
 // JS REACT
-const headerElement = React.createElement('h1', {}, 'Hello World!');
-const secondHeaderElement = React.createElement('h2', {}, 'The Best Frontend Framework Ever!!')
-const headerSectionElement = React.createElement('header', {className: 'site-heading'}, headerElement, secondHeaderElement);
+// const headerElement = React.createElement('h1', {}, 'Hello World!');
+// const secondHeaderElement = React.createElement('h2', {}, 'The Best Frontend Framework Ever!!')
+// const headerSectionElement = React.createElement('header', {className: 'site-heading'}, headerElement, secondHeaderElement);
 
 // JSX REACT
 
+const headerSectionJsx = <header className="site-heading">
+    <h1>Hello World!!!</h1>
+    <h2>The Best Frontend Framework Ever!!</h2>
+</header>
 
-rootReactElement.render(headerSectionElement);
+rootReactElement.render(headerSectionJsx);
 
