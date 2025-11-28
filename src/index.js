098ea24696db904e99b34a0eafd272633b25bbaa
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-28 23:04:24 +0100

    create elements in DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!!!');
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+const rootHtmlElement = document.getElementById('root');
+const rootReactElement = ReactDom.createRoot(rootHtmlElement);
+
+const headerElement = React.createElement('h1', {}, 'Hello World!');
+const secondHeaderElement = React.createElement('h2', {}, 'The Best Frontend Framework Ever!!')
+const headerSectionElement = React.createElement('header', {className: 'site-heading'}, headerElement, secondHeaderElement)
+rootReactElement.render(headerSectionElement);
+
