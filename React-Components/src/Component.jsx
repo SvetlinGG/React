8028eae41c777530c434dce8ec6a99198d8a6e00
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-30 20:44:24 +0100

    replace div to Fragment

--- React-Components/src/Component.jsx
+++ React-Components/src/Component.jsx
@@ -1,7 +1,14 @@
-import { createElement } from "react";
+
 
 export default function Component(){
 
-    return <h1>Hello From Component</h1>
+    return (
+    <Fragment>
+        <h1>Hello From Component</h1>
+        <h2>Hello From my second component</h2>
+    </Fragment>
+        
+    
+    )
 
 }
