6be0aac7b4fc90f546b992f799c8762171687d5a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-30 20:38:54 +0100

    render first component

--- React-Components/src/App.jsx
+++ React-Components/src/App.jsx
@@ -1,14 +1,12 @@
-
-
-import './App.css'
+import Component from "./Component"
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <main>
+      <Component />
+    </main>
   )
 }
 
---
+++ React-Components/src/Component.jsx
@@ -0,0 +1,7 @@
+import { createElement } from "react";
+
+export default function Component(){
+
+    return <h1>Hello From Component</h1>
+
+}
