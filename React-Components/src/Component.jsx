4cc5ab3196eadc9ae0549e6c2c9c388b54b6c796
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-30 20:47:16 +0100

    use empty tags

--- React-Components/src/Component.jsx
+++ React-Components/src/Component.jsx
@@ -3,10 +3,10 @@
 export default function Component(){
 
     return (
-    <Fragment>
+    <>
         <h1>Hello From Component</h1>
         <h2>Hello From my second component</h2>
-    </Fragment>
+    </>
         
     
     )
