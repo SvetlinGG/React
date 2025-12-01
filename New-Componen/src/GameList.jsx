692b5c1828df5eb29be41ea1c13f9b8b9b3d73c8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-01 15:27:18 +0100

    make a list with games and properties

--- New-Componen/src/GameList.jsx
+++ New-Componen/src/GameList.jsx
@@ -1,4 +1,4 @@
-
+import GameListItem from '../src/GameListItem'
 export default function GameList(){
 
     return (
@@ -6,9 +6,9 @@ export default function GameList(){
         <h1>Game List</h1>
         <hr />
         <ul>
-            <li>Need For Speed </li>
-            <li>Game of Thrones</li>
-            <li>War Kraft</li>
+            <GameListItem title="Need For Speed" src="/public/1311158.jpeg" />
+            <GameListItem title="Game Of Thrones" src="/public/OIP.jpeg" />
+            <GameListItem title="War Kraft" src="/public/world-of-warcraft-1920x1080-d2oib3m87tnra9oo.jpg" />
         </ul>
         </>
     )
---
+++ New-Componen/src/GameListItem.jsx
@@ -0,0 +1,7 @@
+
+export default function gameListItem(props){
+
+    return (
+        <ul >{props.title} - {props.src}</ul>
+    )
+}
