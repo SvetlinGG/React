89ee73383cfd0ed8b99e16cf8c05bb282b4abb1d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-02 00:43:34 +0100

    show and hide button

--- React-Components/src/AdvancedCounter.jsx
+++ React-Components/src/AdvancedCounter.jsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
 export default function AdvancedCounter(){
 
-    const [counter, setCounter] = useState(0)
+    const [show, setShow ] = useState(true);
+    const [counter, setCounter] = useState(0);
+
     const buttonClickHandler = ()=> {
         setCounter(counter + 1)
         
     }
 
+    const hideCounterHandler = () => {
+        setShow(!show);
+    }
+
     return (
         <>
         <h2>Advanced Counter</h2>
-        <p>counter: {counter}</p>
 
-        <button onClick={buttonClickHandler}>Click Me</button>
+        {show
+            ?(
+                <div>
+                    <button onClick={buttonClickHandler}>Click Me</button>
+                    <button onClick={hideCounterHandler}>Hide Counter</button>
+                </div>
+            )
+            : <p>Counter is Hidden</p>
+        }
+        <button onClick={hideCounterHandler}>{show ? 'Hide' : 'Show'} Counter</button>
+
+        
         </>
-    )
+    );
 }
- 
