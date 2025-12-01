65601f14cab4138d81a8c7775c27f84538af3cbb
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-01 15:06:40 +0100

    make a game list

---
+++ New-Componen/src/GameList.jsx
@@ -0,0 +1,13 @@
+
+export default function GameList(){
+
+    return (
+        <>
+        <ul>
+            <li>Need For Speed</li>
+            <li>Game of Thrones</li>
+            <li>War Kraft</li>
+        </ul>
+        </>
+    )
+}
