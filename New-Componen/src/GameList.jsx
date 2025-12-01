44bc080b17c72cdf8923e5b24c39f7b1ede24ab2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-01 15:45:12 +0100

    setup picture on component

--- New-Componen/src/GameList.jsx
+++ New-Componen/src/GameList.jsx
@@ -7,7 +7,11 @@ export default function GameList(){
         <h1>Game List</h1>
         <hr />
         <ul>
-            <GameListItem title="Need For Speed" src="/public/1311158.jpeg" />
+            <div>
+                <GameListItem title="Need For Speed" />
+                <img className="need" src="/public/1311158.jpeg" alt="need" />
+            </div>
+            
             <GameListItem title="Game Of Thrones" src="/public/OIP.jpeg" />
             <GameListItem title="War Kraft" src="/public/world-of-warcraft-1920x1080-d2oib3m87tnra9oo.jpg" />
         </ul>
--- New-Componen/src/GameListItem.jsx
+++ New-Componen/src/GameListItem.jsx
@@ -2,6 +2,6 @@
 export default function gameListItem(props){
 
     return (
-        <ul >{props.title} - {props.src}</ul>
+        <ul style={{ width: '20px', height: '20px'}}>{props.title} - {props.img}</ul>
     )
 }
