157851e708607c96d5ddf32ab18ae5e7eeeed5c0
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-04 22:37:18 +0100

    add a new movie in to the list

--- Component-Props/src/MovieList.jsx
+++ Component-Props/src/MovieList.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import MovieListItem from "./MovieListItem";
 
 export default function MovieList(){
 
-    const movies = [
+    const [movies, setMovies] = useState([
         {title: 'Iron Man', year: 2020},
         {title: 'The Matrix', year: 1999},
         {title: 'Star Wars', year: 1986},
         {title: 'The Case for Christ', year: 2001},
-    ]
+    ]);
+
+   
+
+    const addButtonHandler = () => {
+        setMovies([
+            {title: 'Amazing Grace', year: 2020}
+        ])
+    }
 
     return (
         <>
@@ -15,9 +24,10 @@ export default function MovieList(){
 
         <ul>
             {movies.map( movie => <MovieListItem title={movie.title} year={movie.year}/>)}
-            
-            
         </ul>
+        <button onClick={addButtonHandler}>Add</button>
+
+
         </>
     )
 }
