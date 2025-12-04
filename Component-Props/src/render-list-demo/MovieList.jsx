1a6997bd2a118827a2b627b9a986c30e57c31cfa
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-04 23:19:32 +0100

    make new folder

--- Component-Props/src/render-list-demo/MovieList.jsx
+++ Component-Props/src/render-list-demo/MovieList.jsx
@@ -4,20 +4,19 @@ import MovieListItem from "./MovieListItem";
 export default function MovieList(){
 
     const [movies, setMovies] = useState([
-        {title: 'Iron Man', year: 2020},
-        {title: 'The Matrix', year: 1999},
-        {title: 'Star Wars', year: 1986},
-        {title: 'The Case for Christ', year: 2001},
+        {id: 1, title: 'Iron Man', year: 2020},
+        {id: 2, title: 'The Matrix', year: 1999},
+        {id: 3, title: 'Star Wars', year: 1986},
+        {id: 4, title: 'The Case for Christ', year: 2001},
     ]);
 
    
 
     const addButtonHandler = () => {
         setMovies( oldState => [
-            ...oldState, {title: 'Amazing Grace', year: 2020}
-        ]
-            //{title: 'Amazing Grace', year: 2020}
-        )
+            {title: 'Amazing Grace', year: 2020},
+            ...oldState,
+        ]);
     }
 
     return (
@@ -25,7 +24,7 @@ export default function MovieList(){
         <h2>Movie List</h2>
 
         <ul>
-            {movies.map( movie => <MovieListItem title={movie.title} year={movie.year}/>)}
+            {movies.map( movie => <MovieListItem key={movie.id} title={movie.title} year={movie.year}/>)}
         </ul>
         <button onClick={addButtonHandler}>Add</button>
 
