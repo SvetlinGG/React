a403e4ac6161dbf389d8f8c5da7d6897f4955135
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-08 22:44:06 +0100

    useEffect with fetch

--- Component-Props/src/fetch-demo/CharacterList.jsx
+++ Component-Props/src/fetch-demo/CharacterList.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CharacterList(){
 
     const [characters, setCharacters] = useState([])
 
-    fetch('https://swapi.dev/api/people')
-    .then(res => res.json())
-    .then(result => {
-        console.log(result.results);
-        
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people')
+            .then(res => res.json())
+            .then(result => {
+                setCharacters(result.results)
     });
+    },[])
+
+    
 
     return (
 
@@ -17,7 +20,7 @@ export default function CharacterList(){
         <h2>Character List (fetch-demo)</h2>
 
         <ul>
-            {characters.map(character => <li>{character.name}</li>}
+            {characters.map(character => <li key={character.name}>{character.name}</li>)}
         </ul>
         </>
         
