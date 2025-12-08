a034e6736dff25019fea52be039e4881d5d7de6c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-08 22:07:25 +0100

    key handler to activate image area

--- Component-Props/src/key-press-demo/Gallery.jsx
+++ Component-Props/src/key-press-demo/Gallery.jsx
@@ -1,10 +1,17 @@
 export default function Gallery(){
 
+    const onKeyDownHandler = (e)=> {
+        console.log(e);
+        console.log('Key down');
+        
+        
+    }
+
     return (
         <>
         <h2>Gallery Demo (key press)</h2>
 
-        <div style={{backgroundColor: 'lightblue', borderRadius: 20 }}>
+        <div tabIndex={0} onKeyDown={onKeyDownHandler} style={{backgroundColor: 'lightblue', borderRadius: 20 }}>
             <h3>Gallery view</h3>
 
             <img style={{width: 350, height:400}} src="https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg" alt="" />
