4f6439719ffbf057c77d42fdd501fc6a21edf4cc
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-12 01:01:58 +0100

    fix the issue with routing

--- React-Router/src/components/Header.jsx
+++ React-Router/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Header(){
 
