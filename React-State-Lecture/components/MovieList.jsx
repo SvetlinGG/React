327979134fe193f637248e3cdb19a408760c8123
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-13 22:13:18 +0100

    set the key value

--- React-State-Lecture/components/MovieList.jsx
+++ React-State-Lecture/components/MovieList.jsx
@@ -16,8 +16,9 @@ export default function MovieList(){
     const addButtonHandler = () => {
           setMovies(oldState => {
             let newState = [
-                ...oldState, 
-                {id: 5, title: "Amazing Grace", year: '2009'}
+                //...oldState, 
+                {id: 5, title: "Amazing Grace", year: '2009'},
+                ...oldState
             ];
             return newState;
           });
